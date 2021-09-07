5390059d935f56adc9bcbcd4008392d450b834ce
Roux <52894745+sudocode1@users.noreply.github.com>
2021-09-07 20:31:52 +0100

    add support for more methods, add some options, add time etc

--- index.py
+++ index.py
@@ -1,15 +1,35 @@
 import random
 import hashlib
+from datetime import datetime
 
-password = input('enter hash: ')
-#password = '4417a30dc8c6b53f5e2e2b9051159348036017f9061e8ace1f966a1ab58fbedd'
-
+# this isnt particularly efficent but time.time returns something really dumb
+start = int(datetime.utcnow().timestamp())
+attempts = 0
 done = []
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-validHashing = ['sha256']
+validHashing = ['sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'md5']
 finished = False
 
+password = input('hash: ')
+print(f"Supported types: {validHashing}")
+method = input('method: ')
+
+sha1 = hashlib.sha1 
+sha224 = hashlib.sha224
+sha256 = hashlib.sha256 
+sha384 = hashlib.sha384
+sha512 = hashlib.sha512
+md5 = hashlib.md5
+
+if method not in validHashing:
+    print('Not valid')
+    exit()
+
+disableAlreadyTestedLog = True
+disableNotThePasswordLog = True
+
 while finished == False:
+    attempts = attempts + 1
     test = ''
 
     # this code is stupidly inefficent but i wrote it really quickly because i was bored
@@ -20,13 +40,36 @@ while finished == False:
     test = test + random.choice(letters)
 
     finalised = hashlib.sha256(bytes(test, encoding='utf-8')).hexdigest()
+
+    # id use match case but its only available in the beta right now so i wont for compatibility
+    # enjoy the spaghetti code
+    if method == 'sha1':
+        finalised = sha1(bytes(test, encoding='utf-8')).hexdigest()
+    elif method == 'sha224':
+        finalised = sha224(bytes(test, encoding='utf-8')).hexdigest()
+    elif method == 'sha256':
+        finalised = sha256(bytes(test, encoding='utf-8')).hexdigest()
+    elif method == 'sha384':
+        finalised = sha384(bytes(test, encoding='utf-8')).hexdigest()
+    elif method == 'sha512':
+        finalised = sha512(bytes(test, encoding='utf-8')).hexdigest()
+    elif method == 'md5':
+        finalised = md5(bytes(test, encoding='utf-8')).hexdigest()
+
     if test in done:
-        print(f"{test} already found")
+        if disableAlreadyTestedLog == False:
+            print(f"{test} already tested")
+
         continue
     
     if finalised == password:
-        print(f"the password is {test}")
         finished = True
+        print(f"\nPassword: {test}")
+        print(f"Method: {method}")
+        print(f"Attempts: {attempts}")
+        print(f"Time taken (seconds): {(int(datetime.utcnow().timestamp()) - start)}")
     else:
-        #print(f"{test} ({finalised}) is not the password")
+        if disableNotThePasswordLog == False:
+            print(f"{test} ({finalised}) is not the password")
+        
         done.append(test)
